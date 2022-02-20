b21f4d5e05f790cdaaf36dce4191be5d72f78eb2
Doris Nunes <doris90nunes@gmail.com>
2022-02-20 14:46:58 -0300

    feat: adicionado pasta src , arquivo gitignore e package.json na inicialização do projeto.

---
+++ src/index.js
@@ -0,0 +1,44 @@
+const { ApolloServer , gql } = require('apollo-server');
+
+//toda request é POST
+// toda request bate no mesmo endpoint (/graphql)
+
+// Query -> obter informações (GET)
+//Mutation -> Manipular dados (POST,PUT,DELETE)
+//Scalar types -> String, Int,Boolean, Float e ID
+
+const typeDefs = gql `
+
+type User{
+    _id: ID
+    name: String!
+    email: String!
+    active: Boolean!
+}
+
+type Post{
+    _id:ID!
+    title:String!
+    content:String!
+    author:User!
+}
+
+type Query{
+    hello: String
+    users:[User]!
+}
+`;
+const resolvers = {
+    Query: {
+        hello:() => 'Hello Word!',
+        users:() =>[
+            {_id: String(Math.random()), name:'Doris', email:'doris@teste.com', active:true},
+            {_id: String(Math.random()), name:'Debora', email:'debora@teste.com', active:false},
+            {_id: String(Math.random()), name:'Josue', email:'josue@teste.com', active:true},
+        ]
+    }
+};
+
+const server = new ApolloServer({ typeDefs, resolvers});
+server.listen().then(({ url }) => console.log(`Server started at ${url}`));
+
